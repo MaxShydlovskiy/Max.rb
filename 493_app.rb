31cf7caee45e768968142f897815a44abbe29c3e
Max <76831095+MaxShydlovskiy@users.noreply.github.com>
2021-04-14 09:07:57 +0300

    Add files via upload

---
+++ 493_app.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+get( '/form' ) do
+  erb :form
+end
+post ('/convert') do
+  fahrenheit = params ['temperature'].to_f
+  celsius = (fahrenheit — 32) / 1.8
+  format("%0.1f degrees Fahrenheit is %0.1f degrees Celsius.", fahrenheit, celsius)
+end
